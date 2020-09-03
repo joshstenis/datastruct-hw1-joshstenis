757c6ffcec752fedaee784f217b3dab889700e93
joshstenis <joshstenis@gmail.com>
2020-09-02 20:13:05 -0500

    Moved more in the right direction. Currently have questions pending answers from the TA's. Transpose function is farthest along. NOTE: always refer to the document. (Sept. 2)

--- cs2413_hw1.cpp
+++ cs2413_hw1.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
 using namespace std;
 
-class Matrix {
-    int values[][];
-    public:
-        Matrix(int* vals) {
-            this->values = vals;
-        } int* multiply(int* arr) {
-            int product[sizeof(this->values)][sizeof(arr[0])];
-            for(int i=0; i < sizeof(this->values); i++) {
-                for(int j=0; j < sizeof(this->values[i]); j++) {
-                    product[i][j] = this->values[i][j] * arr[i][j];
-                }
-            }
-        } int* transpose() {
-            return 0;
-        } int* sum(int* arr) {
+// class Matrix {
+//     int* values;
+//     public:
+//         Matrix(int* vals) {
+//             this->values = vals;
+//         } int* multiply(int* arr) {
+//             
+//         } int* transpose() {
+//             return 0;
+//         } int* sum(int* arr) {
 
+//         }
+// };
+
+int* strToArray(string str) {
+    int sparse[30];
+    for(int i=0; i < str.length(); i++) {
+        if(str[i] == ' ') {continue;}
+        else {
+            sparse[i] = str[i];
         }
-};
+    }
+}
+
+string arrayToStr(int* arr) {
+    string result;
+    int arrSize = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i < arrSize; i++) {
+        result += arr[i] + " ";
+    } return result.substr(0, result.size()-1);
+}
+
+int* multiply(int*, int*) {
+    int result[30];
+}
+
+int* transpose(int* arr) {
+    int result[40];
+    int arrSize = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i < arrSize; i+3) {        // iterates every row-column-value group -- ASSUMES EFFICIENT REP. -- ALSO ASSUMES I CAN RETURN IN EFFICIENT REP.
+        result[i] = arr[i+1];
+        result[i+1] = arr[i];
+    } return result;
+}
+
+int* sum(int* arr1, int* arr2) {
+    int result[40];
+    for(int i=0; i < sizeof(arr1); i++) {
+        
+    }
+}
 
 int main() {
-    return 0;
+    string rawMatrix;
+    cin >> rawMatrix;
+    int* matrix = strToArray(rawMatrix);
+    // cout << multiply();
 }
