8f0b89da20ebc3604854d4727bfce69ecba5cf44
joshstenis <joshstenis@gmail.com>
2020-09-05 20:08:14 -0500

    Processes work correctly, the problem arises with array transfer from function to function. Also popualateArray() is not populating correct values. (Sept 5)

--- transposeTest.cpp
+++ transposeTest.cpp
@@ -3,21 +3,23 @@ using namespace std;
 
 int x[30];
 int xSize = sizeof(x)/sizeof(x[0]);
+int *zOne;      // same size as x
 
+/**
+ * Prints the contents of
+ */
 void outputMatrix(int arr[]) {
-    int arrSize = sizeof(arr)/sizeof(arr[0]);
-    string output;
-
-    for(int i=0; i < arrSize; i++) {
-        output += arr[i] + " ";
-    } cout << output.substr(0, arrSize-1) << endl;
+    cout << x[0];
+    for(int i=1; i < xSize; i++) {
+        cout << " " << x[i];
+    } cout << endl;
 }
 
 void populateArray(string str, int arr[]) {
-    int xLen = 0;
+    int j = 0;
     for(int i=0; i < str.length(); i++) {
         if(str[i] != ' ')
-            x[xLen++] = str[i];
+            arr[j++] = str[i];
     }
 }
 
@@ -33,24 +35,26 @@ int *copyIntArray(int arr[]) {
 ///////////////
 void transpose(int arr[]) {
     int *tmp = copyIntArray(arr);
+    cout << "ARRAY COPIED -- " << xSize << endl;
 
-    for(int i=0; i < xSize; i+3) {  // iterates every row-column-value group
-        arr[i] = arr[i+1];
-        arr[i+1] = tmp[i];      // swaps the rows and columns as per transposition
+    int j = 0;
+    for(int i=0; i < xSize; i+=3) {  // swaps row and column values for each trio
+        cout << "ITER " << j++ << endl;
+        arr[i] = (arr[i] ^ arr[i+1]) ^ (arr[i+1] = arr[i]);
     }
     cout << "ROW AND COLUMN VALUES SWAPPED" << endl;
 
-    for(int i=0; i < xSize; i+3) {    // bubble sort by row-column-value trios
-        tmp = arr;
-        for(int j=i+3; j < xSize; j+3) {
+    for(int i=0; i < xSize; i+=3) {    // bubble sort by row-column-value trios
+        for(int j=i+3; j < xSize; j+=3) {
+            tmp = &arr[j];
             if(arr[j] < arr[i] || (arr[j] == arr[i] && arr[j+1] < arr[i+1])) {       // if rows OR columns are in wrong order
                 arr[i] = arr[j];
                 arr[i+1] = arr[j+1];
                 arr[i+2] = arr[j+2];
 
-                arr[j] = tmp[j];
-                arr[j+1] = tmp[j+1];
-                arr[j+2] = tmp[j+2];
+                arr[j] = *tmp++;
+                arr[j+1] = *tmp++;
+                arr[j+2] = *tmp++;
             }
         }
     } cout << "ARRAY BUBBLE SORTED" << endl;
@@ -64,8 +68,9 @@ int main() {
     string rawMatrix = "0 0 1 0 1 3 2 2 7 4 2 8";       // Test string
     // getline(cin, rawMatrix);
 
+    cout << x[4] << endl;
     populateArray(rawMatrix, x);
-    cout << "ARRAY POPULATED" << endl;
+    cout << "ARRAY POPULATED -- " << x[4] << endl;
     transpose(x);
     cout << "ARRAY TRANSPOSED" << endl;
 }
