788c592822097a98bcbd6ba8de1ceedd4799d80c
joshstenis <joshstenis@gmail.com>
2020-09-10 13:49:59 -0500

    Hhh

--- cs2413_hw1.cpp
+++ cs2413_hw1.cpp
@@ -93,7 +93,7 @@ void populateArray(int e, int arr[]) {
     for(int i=0; i < e; i++) {
         cin >> num;
         arr[i] = strToInt(num);
-    } cin.ignore(1, ' ');
+    } cin.ignore(2, ' ');
 }
 
 /**
